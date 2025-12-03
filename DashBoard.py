42b0afa504862b7aed4f8427223f32187303a240
HuyPalmer:3 <118195524+Huyddz@users.noreply.github.com>
2025-12-03 15:23:11 +0700

    Rename function and update price history to daily

--- DashBoard.py
+++ DashBoard.py
@@ -20,11 +20,12 @@ def fetch_annual_financials(symbol):
     return stock.financials.T
 
 @st.cache_data
-def fetch_weekly_price_history(symbol):
+def fetch_daily_price_history(symbol):
     stock = yf.Ticker(symbol)
-    return stock.history(period='1y', interval='1wk')
+    return stock.history(period='1d', interval='1h')
 
-st.title('Stock Dashboard')
+#Dashboard
+st.title('Stock Dashboard by SongChiTienQuan')
 symbol = st.text_input('Enter a stock symbol', '')
 
 information = fetch_stock_info(symbol)
@@ -35,9 +36,9 @@ st.subheader(f'Name: {information["longName"]}')
 st.subheader(f'Market Cap: ${information["marketCap"]:,}')
 st.subheader(f'Sector: {information["sector"]}')
 
-price_history = fetch_weekly_price_history(symbol)
-
-st.header('Chart')
+price_history = fetch_daily_price_history(symbol)
+#Daily
+st.header('Daily Chart')
 
 price_history = price_history.rename_axis('Date').reset_index()
 candle_stick_chart = go.Figure(data=[go.Candlestick(x=price_history['Date'],
@@ -48,7 +49,7 @@ candle_stick_chart = go.Figure(data=[go.Candlestick(x=price_history['Date'],
 
 candle_stick_chart.update_layout(xaxis_rangeslider_visible=False)
 st.plotly_chart(candle_stick_chart, use_container_width=True)
-
+#Quarterly and Annual
 quarterly_financials = fetch_quarterly_financials(symbol)
 annual_financials = fetch_annual_financials(symbol)
 
