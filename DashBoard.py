027738f00610d8b63bfcd20e7b349dba44275a76
HuyPalmer:3 <118195524+Huyddz@users.noreply.github.com>
2025-12-04 11:54:29 +0700

    Fix import statement for RESTClient from polygon

--- DashBoard.py
+++ DashBoard.py
@@ -5,7 +5,7 @@ import streamlit as st #Deploy web
 import yfinance as yf #Data collecting from YahooFiance
 import pandas as pd #Graph support
 import polygon
-from polygon.rest import RESTClient #API for auto suggestion (i hope it works)
+from polygon import RESTClient #API for auto suggestion (i hope it works)
 #API key(be careful)
 POLYGON_API_KEY = "WakFbeRcsuUV8Q8ECdZZz32KJaVeu2g2"
 polygon_client = RESTClient(api_key=POLYGON_API_KEY)
